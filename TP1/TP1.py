0c55a31b34f84ff3a5fb866225b39d68516f590f
Gharbi Mohamed Khairallah <lmkg97@gmail.com>
2019-02-13 23:34:30 -0500

    MKG : premier TP1

---
+++ TP1/TP1.py
@@ -0,0 +1,41 @@
+
+########################
+#TODO : 
+#  fontion : creerReseauSocial( , ) : 
+#   Lire les fichiers texte contenant les informations 
+#   et genere le reseau social correspondant. 
+def creerReseauSocial() : 
+
+#
+#  fonction : afficherReseauSocial() 
+#   Affiche le réseau social selon le format présenté en annexe. 
+def afficherReseauSocial() :
+
+# 
+#  fonction : identifierIndividus()
+#   l'agent trouve les noms des deux individues mystères.
+def identifierIndividus() : 
+
+#
+#  fonction : enleverArcsIndesirables( 3 caracteristiques indesirables )
+#   Génère le sous-graphe des caractéristiques désirables.
+def enleverArcsIndesirables() : 
+
+#  fonction : trouverChaineContacts( 2 noms d'individus )
+#   L'agent trouve la meilleure chaine de contacts entre 2 individus à partir 
+#   du sous-graphe des caracteristiques désirables.
+
+def trouverChaineContacts() :  
+
+#  fontion : afficherResultat() 
+#   L'agent présente le résultatde ses accomplissements selon le bon format.
+def afficherResultat() :
+
+# Interface console qui affiche le menu : 
+#   a/ Créer le résea social.
+#   b/ Afficher le réseau social.
+#   c/ Jouer à Qui est-ce ? 
+#   d/ Afficher le résultat.
+#   e/ Quitter. 
+#
+########################
