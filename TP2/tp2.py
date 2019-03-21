d469ed722236a4093cbf9acf313dbda29fba225c
miharbii <fradj.ibrahim@gmail.com>
2019-03-21 17:20:37 -0400

    ajout des commentaire pour les objectifs des fonctions

---
+++ TP2/tp2.py
@@ -0,0 +1,40 @@
+#facile 
+tableauFichier =[]#tableau des fichier données 
+def remplirFichier (nbrFichier):#remplir le tableau avec les fichier 
+    #on devera faire une boucle ou on met les 21 fichier c'est long d'ecrire 21 fichier 
+
+#ef lireFichier():#lire les fichier
+
+#compliquer
+def ouvrirPorte (tableauFichier ): 
+    #cette fonction dois:lire les fichier (utiliser la fonction lireFichier),
+    #crée l'automate , valider le mot de passe, retourne une liste de portes
+
+#moyen
+def affronterLeBoss(Boss.txt):
+    #lis le fichier Boss.txt
+    #generer l'automate associé au chemin decrit dans le fichier 
+    #valider la concatenation des mot de passe des le debut 
+    #trouver le language
+
+#moyen
+def genererAutomate ():#question :est-ce que on utilise cette fonction pour affronterBoss?
+
+#facile
+def afficherLeCheminParcouru():
+    #on a le choix entre :
+    #1 precisant: choix d'une porte,
+    #les mots de passes valide associer a cette porte avec chacune des portes vers lesquelles il mene
+    # si la porte est un gouffre et force l'agent a recommencer le labyrunthe
+    # ou bien 2 le choix du boss
+    # la concatenation des mot de passe depuis l'entre du labyrinthe pour le boss 
+    #et le langage universel reconu par le boss
+    #si le boss est vaincu ou non (si c'est non force l'agent a recommencer)
+
+#facile
+def afficher():
+    #interface console qui affiche :
+    #entrer dans le labyrinthe (porte1)
+    #ouvrir une porte
+    #afficher le chemin parcouru (fonction qu'on a deja fait)
+    #quitter
